9d0bc320f4234f2eecaec6d9ec6caeed8e31432c
Michael <cold.bubbling.tea@gmail.com>
2025-03-06 14:17:11 +0800

    feat: Implement agents' File model

--- models/agents.py
+++ models/agents.py
@@ -1,14 +1,35 @@
 from beanie import Document
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class File(BaseModel):
+    """
+    Attributes
+        name (str): File name
+        text (str): Extracted text
+        tokens (int): Tokens utilized by the text
+    """
+    name: str
+    text: str
+    tokens: int = Field(default=0)
 
 class CreateAgent(BaseModel):
-    """Schema for creating a new Agent"""
+    """
+    Attributes
+        name (str): Name of the Agent
+    """
     name: str
 
 class AgentDB(Document):
-    """Agent document stored in MongoDB"""
+    """
+    Attributes
+        name (str): Name of the Agent
+        files (list[File]): Files to access
+        messages (list[str]): All prompts by user
+    """
     name: str
-    messages: list[str] = []
+    files: List[File] = Field(default=[])
+    messages: List[str] = Field(default=[])
     
     class Settings:
         name = "agents"
